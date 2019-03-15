93f87116736f8a890c5ea9c0cc1f65df229d8d68
Antonis <antonistodoris@gmail.com>
2019-03-15 09:31:21 +0100

    Made the API that returns a list of items

--- code/item.py
+++ code/item.py
@@ -108,10 +108,10 @@ class ItemList(Resource):
 
     query = "SELECT * FROM items"
     result = cursor.execute(query)
-    items = result.fetchall()
+    items = []
+    for row in result:
+      items.append({'name': row[0], 'price': row[1]})
 
-    connection.commit()
     connection.close()
 
     return {'items': items}
-
