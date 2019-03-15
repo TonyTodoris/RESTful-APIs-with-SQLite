224561c83be9838507fe98d5505606c34fd41c84
Antonis <antonistodoris@gmail.com>
2019-03-15 09:57:59 +0100

    extend JWT token

--- code/app.py
+++ code/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt import JWT
 
-from security import authenticate, identity
+from security import authenticate, identity as identity_function
+from datetime import timedelta
 
 from user import UserRegister
 from item import Item, ItemList
@@ -11,7 +12,26 @@ app = Flask(__name__)
 app.secret_key = 'Antonis'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWT(app, authenticate, identity_function) # /login
+
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800) # config JWT to expire within half an hour
+app.config['JWT_AUTH_USERNAME_KEY'] = 'username' # config JWT auth key name to be 'email' instead of default 'username'
+
+#customize JWT auth response, include user_id in response body
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+                        'access_token': access_token.decode('utf-8'),
+                        'user_id': identity.id
+                   })
+
+@jwt.jwt_error_handler
+def customized_error_handler(error):
+    return jsonify({
+                       'message': error.description,
+                       'code': error.status_code
+                   }), error.status_code
 
 api.add_resource(Item, '/item/<string:name>') #http://localhost:5000/student/Antonis
 api.add_resource(ItemList, '/items')
